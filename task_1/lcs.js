dd80e8748c2c6cb418e30333182a4551d9e47cf7
Mahfuz <mahfughasanbd@gmail.com>
2025-03-11 11:23:32 +0600

    Task 1 is done successfully

--- task_1/lcs.js
+++ task_1/lcs.js
@@ -1,30 +1,29 @@
-function findLCS(strings) {
+function LCS(strings) {
     if (strings.length === 0) return "";
     if (strings.length === 1) return strings[0];
 
-    let shortest = strings.reduce((a, b) => (a.length <= b.length ? a : b)); // Find the shortest string
+    let SmallestString = strings.reduce((a, b) =>
+        a.length <= b.length ? a : b
+    );
     let lcs = "";
-    // console.log(shortest);
 
-    for (let i = 0; i < shortest.length; i++) {
-        for (let j = i + 1; j <= shortest.length; j++) {
-            let substr = shortest.substring(i, j);
+    for (let i = 0; i < SmallestString.length; i++) {
+        for (let j = i + 1; j <= SmallestString.length; j++) {
+            let substr = SmallestString.substring(i, j);
             if (
-                strings.every((str) => str.includes(substr)) &&
+                strings.every((s) => s.includes(substr)) &&
                 substr.length > lcs.length
             ) {
                 lcs = substr;
             }
         }
     }
-
     return lcs;
 }
 
-// Get input from cozzzmmand-line arguments
 const args = process.argv.slice(2);
 if (args.length < 2) {
-    console.log(args.length === 1 ? args[0] : ""); // If one string is given, return it; if none, return empty
+    console.log(args.length === 1 ? args[0] : "");
 } else {
-    console.log(findLCS(args));
+    console.log(LCS(args));
 }
